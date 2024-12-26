c86036df1117acc661bf46d40099a5eea35a8d8b
TarasFirst <firsttaras@gmail.com>
2024-12-26 10:18:46 +0100

    create scrape_one_product

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from urllib.parse import urljoin
 
+from bs4 import Tag
 
 BASE_URL = "https://webscraper.io/"
 HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
@@ -13,6 +14,17 @@ class Product:
     price: float
     rating: int
     num_of_reviews: int
+
+
+def scrape_one_product(product: Tag):
+    return Product(
+        title=product.select_one(".title")["title"],
+        description=product.select_one(".description").text,
+        price=float(product.select_one(".price").text.lstrip("$")),
+        rating=int(product.select_one("data-rating").text),
+        num_of_reviews=int(product.select_one(".review-count").text.split(" ")[0])
+    )
+
 
 
 def get_all_products() -> None:
